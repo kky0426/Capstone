4dd3d6eda4a57c9f91f878ef0fd7b9cdbe3fe8a9
Im Hyuk <cs.javah@kakao.com>
2021-03-18 23:01:50 +0900

    수정2..

--- 임혁/ktor-lol/src/main/kotlin/Summoner.kt
+++
@@ -1,9 +0,0 @@
-data class Summoner(
-    val id: String,
-    val accountId: String,
-    val puuid: String,
-    val name: String,
-    val profileIconId: Long,
-    val revisionDate: Long,
-    val summonerLevel: Long
-)
